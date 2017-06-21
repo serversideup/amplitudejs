fc48de93b71438c743b9c765159bdceb2a9632f8
Jay Rogers <jay@521dimensions.com>
2017-06-21 17:37:56 -0500

    Properly built file

--- dist/amplitude.js
+++ dist/amplitude.js
@@ -4608,7 +4608,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; } /*
                                                                                                                                                                                                                   	Amplitude.js
-                                                                                                                                                                                                                  	Version: 	3.0
+                                                                                                                                                                                                                  	Version: 	3.1.0
                                                                                                                                                                                                                   	Author: 	Dan Pastori
                                                                                                                                                                                                                   	Company: 	521 Dimensions
                                                                                                                                                                                                                   */
